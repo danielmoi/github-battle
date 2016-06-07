bd5d23613d4f64fab9c64065c8061183730bda62
danielmoi <daniel@ampersandmoi.com>
2016-06-07 14:03:46 +1000

    Create Avatar component with 3 nested components

--- app/index.js
+++ app/index.js
@@ -1,15 +1,53 @@
+var USER_DATA = {
+  name: 'Daniel Moi',
+  username: 'danielmoi',
+  image: 'https://avatars0.githubusercontent.com/u/9400516?v=3&s=460'
+};
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
   render: function() {
     return (
-      <div>Hello {this.props.name}</div>
+      <img src={this.props.imageUrl} style={{ height: 100, width: 100 }} />
     )
   }
 });
 
+var ProfileLink = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <a href={'https://github.com/' + this.props.username}>
+          { this.props.username }
+        </a>
+      </div>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function() {
+    return (
+      <div> { this.props.name } </div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ProfilePic imageUrl={ this.props.user.image } />
+        <ProfileName name={ this.props.user.name } />
+        <ProfileLink username={ this.props.user.username } />
+      </div>
+    )
+  }
+})
+
 ReactDOM.render(
-  <HelloWorld name='Daniel' />,
+  <Avatar user={ USER_DATA } />,
   document.getElementById('app')
 );
