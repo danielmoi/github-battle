436195f47e11aefcbf2c3ca1c382328b4d589efc
danielmoi <daniel@ampersandmoi.com>
2016-06-07 17:24:38 +1000

    Remove hashHistory code from routes

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,18 +4,15 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
 
-// var hashHistory = ReactRouter.hashHistory;
 var browserHistory = ReactRouter.browserHistory;
 
 var Main = require('../components/Main');
 var Home = require('../components/Home');
 
 var routes = (
-  // <Router history={ hashHistory }>
   <Router history={ browserHistory }>
     <Route path='/' component={ Main }>
-      {/*<IndexRoute component={ Home }></IndexRoute>*/}
-      <Route path='home' component={ Home }></Route>
+      <IndexRoute component={ Home }></IndexRoute>
     </Route>
 
   </Router>
--- webpack.config.js
+++ webpack.config.js
@@ -27,9 +27,6 @@ module.exports = {
   ],
   devServer: {
     port: 3000,
-    // historyApiFallback: {
-    //   index: 'index.html'
-    // }
     historyApiFallback: true
   }
 }
