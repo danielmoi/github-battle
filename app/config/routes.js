2f9531a93fe76a9d4af69607696a26281916056d
danielmoi <daniel@ampersandmoi.com>
2016-06-07 16:22:53 +1000

    Attempt to implement browserHistory Left code as comment

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,11 +4,14 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
 
+var hashHistory = ReactRouter.hashHistory;
+// var browserHistory = ReactRouter.browserHistory;
+
 var Main = require('../components/Main');
 var Home = require('../components/Home');
 
 var routes = (
-  <Router>
+  <Router history={ hashHistory }>
     <Route path='/' component={ Main }>
       <Route path='/home' component={ Home }></Route>
     </Route>
