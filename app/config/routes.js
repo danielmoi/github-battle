3c889bfcdf871c09ec4887b87daceba41c516159
danielmoi <daniel@ampersandmoi.com>
2016-06-08 13:17:58 +1000

    Setup battle route

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,12 +3,14 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
-var PromptContainer = require('../containers/PromptContainer');
 
 var browserHistory = ReactRouter.browserHistory;
 
 var Main = require('../components/Main');
 var Home = require('../components/Home');
+
+var PromptContainer = require('../containers/PromptContainer');
+var ConfirmBattleContainer = require('../containers/ConfirmBattleContainer');
 
 var routes = (
   <Router history={ browserHistory }>
@@ -17,6 +19,7 @@ var routes = (
       <Route path='playerOne' header='Player One' component={ PromptContainer }></Route>
       <Route path='playerTwo/:playerOne' header='Player Two' component={ PromptContainer }></Route>
     </Route>
+    <Route path='battle' component={ ConfirmBattleContainer }></Route>
 
   </Router>
 );
