788f2cf9da93b046fdd3bd1422e436d60bd80e70
danielmoi <daniel@ampersandmoi.com>
2016-06-08 10:51:37 +1000

    Implement username state of PromptContainer Retrieve value from input, update state

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -2,6 +2,16 @@ var React = require('react');
 var transparentBg = require('../styles').transparentBg;
 
 var PromptContainer = React.createClass({
+  getInitialState: function() {
+    return {
+      username: ''
+    }
+  },
+  _onUpdateUser: function(e) {
+    this.setState({
+      username: e.target.value
+    });
+  },
   render: function() {
     return (
       <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={ transparentBg }>
@@ -9,7 +19,13 @@ var PromptContainer = React.createClass({
         <div className="col-sm-12">
           <form>
             <div className="form-group">
-              <input type="text" placeholder="GitHub username" className="form-control"/>
+              <input
+                type="text"
+                placeholder="GitHub username"
+                className="form-control"
+                onChange={ this._onUpdateUser }
+                value={ this.state.username }
+                />
             </div>
             <div className="form-group col-sm-4 col-sm-offset-4">
               <button className="btn btn-block btn-success" type="submit">
