5b359a9fcc27657fa9bfd9cde859200fb0cfaa30
danielmoi <daniel@ampersandmoi.com>
2016-06-08 10:25:59 +1000

    Content and style for home page

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,9 +1,13 @@
 var React = require('react');
+var transparentBg = require('../styles').transparentBg;
 
 var Home = React.createClass({
   render: function() {
     return (
-      <div>Hello from Home!</div>
+      <div className="jumbotron col-sm-12 text-center" style={ transparentBg }>
+        <h1>GitHub Battle</h1>
+        <p className="lead">Some fancy motto</p>
+      </div>
     )
   }
 });
