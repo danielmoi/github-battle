caae2b4dc26fc6864c89d59c0735756266537e06
danielmoi <daniel@ampersandmoi.com>
2016-06-08 13:09:04 +1000

    Start ConfirmBattle components

---
+++ app/components/ConfirmBattle.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ConfirmBattle = require('../components/ConfirmBattle');
+
+function ConfirmBattle(props) {
+  return (
+    <ConfirmBattle></ConfirmBattle>
+  )
+}
+
+module.exports = ConfirmBattle;
