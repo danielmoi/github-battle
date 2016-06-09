ecd093f31effd1aa6a95e1620394ac77c405c133
danielmoi <daniel@ampersandmoi.com>
2016-06-09 11:05:46 +1000

    Add prop types (using shape) for UserDetails

--- app/components/UserDetails.js
+++ app/components/UserDetails.js
@@ -63,7 +63,17 @@ function UserDetails(user) {
 
 UserDetails.propTypes = {
   score: PropTypes.number,
-  info: PropTypes.object.isRequired
+  info: PropTypes.shape({
+    avatar_url: PropTypes.string.isRequired,
+    blog: PropTypes.string,
+    company: PropTypes.string,
+    followers: PropTypes.number.isRequired,
+    following: PropTypes.number.isRequired,
+    location: PropTypes.string,
+    login: PropTypes.string.isRequired,
+    name: PropTypes.string,
+    public_repos: PropTypes.number.isRequired
+  })
 }
 
 module.exports = UserDetails;
