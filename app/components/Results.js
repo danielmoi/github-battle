a6f51d79d56ae2fee8ec233800a6d12ce1050dfc
danielmoi <daniel@ampersandmoi.com>
2016-06-09 13:48:00 +1000

    Populate Results with winner and lower data

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,5 +1,8 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
+var styles = require('../styles');
+var UserDetails = require('../components/UserDetails');
+var UserDetailsWrapper = require('../components/UserDetailsWrapper');
 
 
 function puke(obj) {
@@ -9,8 +12,30 @@ function puke(obj) {
 }
 
 function Results(props) {
+  var winningIndex = props.scores[0] > props.scores[1] ? 0 : 1;
+  var losingIndex = winningIndex === 0 ? 1 : 0;
+
   return (
-    <div>RESULTS: { puke(props) }</div>
+    <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+      <h1>Results</h1>
+      <div className="col-sm-8 col-sm-offset-2">
+
+        <UserDetailsWrapper header="Winner">
+          <UserDetails
+            score={ props.scores[winningIndex]}
+            info={ props.playersInfo[winningIndex] } />
+        </UserDetailsWrapper>
+
+        <UserDetailsWrapper header="Loser">
+          <UserDetails
+            score={ props.scores[losingIndex ]}
+            info={ props.playersInfo[losingIndex] } />
+        </UserDetailsWrapper>
+
+
+      </div>
+
+    </div>
   )
 }
 
