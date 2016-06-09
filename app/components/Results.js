0c6e3ddaf09dd2b513ab75c7708f7d1b2c8a99cc
danielmoi <daniel@ampersandmoi.com>
2016-06-09 14:03:45 +1000

    Implement tie result UI

--- app/components/Results.js
+++ app/components/Results.js
@@ -13,6 +13,36 @@ function puke(obj) {
 }
 
 function Results(props) {
+
+  if (props.scores[0] === props.scores[1] ) {
+    return (
+      <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+        <h1>It's a tie!</h1>
+
+        <div className="col-sm-8 col-sm-offset-2">
+          <UserDetailsWrapper>
+            <UserDetails
+              score={ props.scores[0]}
+              info={ props.playersInfo[0] } />
+          </UserDetailsWrapper>
+
+          <UserDetailsWrapper>
+            <UserDetails
+              score={ props.scores[1 ]}
+              info={ props.playersInfo[1] } />
+          </UserDetailsWrapper>
+        </div>
+
+        <div className="col-sm-12" style={ styles.space }>
+          <Link to="/playerOne" >
+            <button type="button" className="btn btn-lg btn-danger">Start Over</button>
+          </Link>
+        </div>
+
+      </div>
+    )
+  }
+
   var winningIndex = props.scores[0] > props.scores[1] ? 0 : 1;
   var losingIndex = winningIndex === 0 ? 1 : 0;
 
